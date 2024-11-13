00f7e4d8be261b4eee040d1d04c45a6d35a393fd
HP <HP@DESKTOP-N19O9LD>
2024-11-13 09:18:53 +0530

    pushed

---
+++ DemoJenkinsASAD/src/test/java/ContactTest/ToCreateContactsTest_007.java
@@ -0,0 +1,9 @@
+package ContactTest;
+
+public class ToCreateContactsTest_007
+{
+	public static void main (String[]args) {
+	System.out.println("Hi Hello");
+
+	}
+}
---
+++ DemoJenkinsASAD/src/test/java/OrganizationTest/ToCreateOrganization_007Test.java
@@ -0,0 +1,9 @@
+package OrganizationTest;
+
+public class ToCreateOrganization_007Test {
+
+	public static void main (String[]args) {
+	System.out.println("Bye");	
+
+}
+}
